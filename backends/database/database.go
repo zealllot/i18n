160885dcb83d210a8b88f818bd9a3d28faf4160b
Zealllot <502372679@qq.com>
2019-03-13 16:10:10 +0800

    change locale size from 12 to 50

--- backends/database/database.go
+++ backends/database/database.go
@@ -9,11 +9,11 @@ import (
 
 // Translation is a struct used to save translations into databae
 type Translation struct {
-	Locale      string `sql:"size:12;"`
+	Locale      string `sql:"size:50;"`
 	Key         string `sql:"size:4294967295;"`
 	Value       string `sql:"size:4294967295"`
-	DisplayId   string `sql:"size:12;"`
-	Description string `sql:"size:4294967295"`
+	DisplayId   string
+	Description string
 }
 
 // New new DB backend for I18n
